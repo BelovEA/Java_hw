8857f6a098bd4e01b5a6fc90e1057704184829d3
BelovEA <vrachea676@gmail.com>
2023-06-14 22:09:07 +0300

    hw_2 is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("name", "Ivanov");
+        params.put("country", "Russia");
+        params.put("city", "Moscow");
+        params.put("age", null);
+        System.out.println("select * from students where " + getQuery(params));
+    }
+
+    public static String getQuery(Map<String, String> param) {
+        StringBuilder s = new StringBuilder();
+        for (Map.Entry<String, String> pair: param.entrySet())
+        {
+            if(pair.getValue() != null)
+            {
+                s.append(pair.getKey() + " = " + pair.getValue() + " and ");
+            }
+        }
+        s.delete(s.toString().length()-5,s.toString().length());
+        return s.toString();
+
+
+
     }
 }
