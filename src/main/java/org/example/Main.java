056d3f80fbeb810c74bfbc57e2ffe15db8907b42
BelovEA <vrachea676@gmail.com>
2023-06-14 22:10:25 +0300

    hw_2 is done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 
 import java.util.HashMap;
 import java.util.Map;
-
+//hw_2
 public class Main {
     public static void main(String[] args) {
         Map<String, String> params = new HashMap<String, String>();
